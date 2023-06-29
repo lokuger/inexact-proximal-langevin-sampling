fix problem with accuracy scaling constant being recomputed for every sample. Need to test new values for accuracy levels

example_deblur_tv.py
@@ -21,16 +21,17 @@ import distributions as pds
 
 #%% initial parameters: test image, computation settings etc.
 params = {
-    'iterations': 10000,
-    'testfile_path': 'test_images/flintstones.png',
+    'iterations': 500,
+    'testfile_path': 'test-images/flintstones.png',
     'blurtype': 'gaussian',
-    'bandwidth': 1,
-    'noise_std': 0.20,
-    'log_epsilon': None,
+    'bandwidth': 1.5,
+    'noise_std': 0.1,
+    'log_epsilon': -6.0,
     'iter_prox': 10,
     'step': 'large',
     'efficient': True,
-    'verbose': True
+    'verbose': True,
+    'result_root': './results/denoise-tv',
     }
 
 #%% auxiliary functions
@@ -121,24 +122,24 @@ def main():
     #%% Ground truth
     # results_file = results_dir+'/result_images.npy'
     if True: #not os.path.exists(results_file): 
-        rng = default_rng(13928696)
+        rng = default_rng(1392)
         verb = params['verbose']
-        # try:
-        #     x = io.imread(params['testfile_path'],as_gray=True).astype(float)
-        # except FileNotFoundError:
-        #     print('Provided test image did not exist under that path, aborting.')
-        #     sys.exit()
-        # # handle images that are too large
-        # Nmax = 128
-        # if x.shape[0] > Nmax or x.shape[1] > Nmax: x = transform.resize(x, (Nmax,Nmax))
+        try:
+            x = io.imread(params['testfile_path'],as_gray=True).astype(float)
+        except FileNotFoundError:
+            print('Provided test image did not exist under that path, aborting.')
+            sys.exit()
+        # handle images that are too large
+        Nmax = 256
+        if x.shape[0] > Nmax or x.shape[1] > Nmax: x = transform.resize(x, (Nmax,Nmax))
         
         # chess test image
-        n,t = 32, 8
-        x = np.zeros((n,n))
-        for i in np.arange(32):
-            for j in np.arange(32):
-                if (i//t)%2 == (j//t)%2:
-                    x[i,j] = 1
+        # n,t = 64, 16
+        # x = np.zeros((n,n))
+        # for i in np.arange(n):
+        #     for j in np.arange(n):
+        #         if (i//t)%2 == (j//t)%2:
+        #             x[i,j] = 1
         
         x = x-np.min(x)
         x = x/np.max(x)
@@ -166,8 +167,8 @@ def main():
         L = max_ev_ata/noise_std**2
         
         # show ground truth and corrupted image
-        # my_imshow(x,'ground truth')
-        # my_imshow(y,'noisy image')
+        my_imshow(x,'ground truth')
+        my_imshow(y,'noisy image')
         
         #%% SAPG - compute the optimal regularization parameter
         # unscaled_posterior = pds.l2_deblur_tv(n, n, a, at, y, noise_std=noise_std, mu_tv=1)
@@ -206,7 +207,7 @@ def main():
         
         #%% regularization parameter
         # mu_tv = s.mean_theta[-1]          # computed by SAPG
-        mu_tv = 2.1                       # set by hand, tuned for best MAP PSNR
+        mu_tv = 1.85                       # set by hand, tuned for best MAP PSNR
             
         #%% MAP computation - L2-TV deblurring
         # deblur using PDHG in the version f(Kx) + g(x) + h(x) with smooth h
@@ -214,7 +215,7 @@ def main():
         # this matches example 5.7 - PD-explicit in Chambolle+Pock 2016
         x0_pd, y0_pd = np.zeros(x.shape), np.zeros((2,)+x.shape)
         tau_pd, sigma_pd = 1/(np.sqrt(8)+L), 1/np.sqrt(8)
-        n_iter_pd = 2000
+        n_iter_pd = 500
         f = pot.l2_l1_norm(n, n, scale=mu_tv)
         k,kt = tv._imgrad, tv._imdiv
         g = pot.zero()
@@ -225,7 +226,7 @@ def main():
         u = pd.compute(verbose=True)
         if verb: sys.stdout.write('Done.\n'); sys.stdout.flush()
         
-        # my_imshow(u,'MAP estimate')
+        my_imshow(u,'MAP estimate')
         print('MAP: mu_TV = {:.1f};\tPSNR: {:.2f}'.format(mu_tv,10*np.log10(np.max(x)**2/np.mean((u-x)**2))))
             
         #%% sample using inexact PLA
@@ -234,10 +235,11 @@ def main():
             tau = 1/L
         elif params['step'] == 'small':
             tau = 0.5/L
-        iter_prox = params['iter_prox']
-        epsilon_prox = 10**params['log_epsilon'] if params['log_epsilon'] is not None else None
-        n_samples = params['iterations']
-        burnin = 1000
+        iter_prox = np.Inf #params['iter_prox']
+        C = tau*mu_tv*tv(u)
+        epsilon_prox = C * 10**params['log_epsilon'] if params['log_epsilon'] is not None else None
+        burnin = 200
+        n_samples = params['iterations']+burnin
         posterior = pds.l2_deblur_tv(n, n, a, at, max_ev_ata, y, noise_std=noise_std, mu_tv=mu_tv)
         eff = params['efficient']
         
@@ -245,19 +247,21 @@ def main():
         ipla = inexact_pla(x0, n_samples, burnin, posterior, step_size=tau, rng=rng, epsilon_prox=epsilon_prox, iter_prox=iter_prox, efficient=eff)
         if verb: sys.stdout.write('Sample from posterior - '); sys.stdout.flush()
         ipla.simulate(verbose=verb)
-        if verb: sys.stdout.write('Done.\n'); sys.stdout.flush()
         
         #%% plots
         # diagnostic plot, making sure the sampler looks plausible
-        # plt.plot(np.arange(1,n_samples+1), ipla.logpi_vals)
-        # plt.title('- log(pi(X_n)) = F(K*X_n) + G(X_n) [All]')
-        # plt.show()
+        plt.plot(np.arange(1,n_samples+1), ipla.logpi_vals)
+        plt.title('- log(pi(X_n)) = F(K*X_n) + G(X_n) [All]')
+        plt.show()
         # plt.plot(np.arange(50+1,n_samples+1), ipla.logpi_vals[50:])
         # plt.title('- log(pi(X_n)) = F(K*X_n) + G(X_n) [after burn-in]')
         # plt.show()
         # plt.plot(np.arange(burnin+1,n_samples+1), ipla.logpi_vals[burnin:])
         # plt.title('- log(pi(X_n)) = F(K*X_n) + G(X_n) [after burn-in]')
         # plt.show()
+        plt.plot(np.arange(1,n_samples+1), ipla.dgap_vals)
+        plt.title('duality gap values')
+        plt.show()
         
         my_imshow(ipla.mean, 'Sample Mean, log10(epsilon)={}'.format(params['log_epsilon']))
         logstd = np.log10(ipla.std)

example_denoise_tv.py
@@ -20,10 +20,10 @@ import distributions as pds
 
 #%% initial parameters: test image, computation settings etc.
 params = {
-    'iterations': 100000,
+    'iterations': 100,
     'testfile_path': 'test-images/wheel.png',
     'noise_std': 0.2,
-    'log_epsilon': 0.0,
+    'log_epsilon': -1.0,
     'step': 'large',
     'efficient': True,
     'verbose': True,
@@ -132,7 +132,7 @@ def main():
             
         #%% MAP computation - L2-TV denoising (ROF)
         if verb: sys.stdout.write('Compute MAP - '); sys.stdout.flush()
-        u,its_map = tv.inexact_prox(y, gamma=mu_tv*noise_std**2, epsilon=1e-8, max_iter=500, verbose=verb)
+        u,its_map,_ = tv.inexact_prox(y, gamma=mu_tv*noise_std**2, epsilon=1e-8, max_iter=100, verbose=verb)
         if verb: sys.stdout.write('Done.\n'); sys.stdout.flush()
         
         my_imshow(u,'MAP (dual aGD, mu_TV = {:.1f})'.format(mu_tv))
@@ -145,9 +145,10 @@ def main():
             tau = 1/L
         elif params['step'] == 'small':
             tau = 0.5/L
-        epsilon = 10**params['log_epsilon']
-        n_samples = params['iterations']
+        C = tau*mu_tv*tv(u)
+        epsilon = C*(10**params['log_epsilon'])
         burnin = 50 # burnin for denoising is usually short
+        n_samples = params['iterations']+burnin
         posterior = pds.l2_denoise_tv(n, n, y, noise_std=noise_std, mu_tv=mu_tv)
         eff = params['efficient']
         
@@ -184,7 +185,7 @@ def main():
         my_imshow(y, 'noisy')
         my_imshow(u, 'map')
         my_imshow(mn, 'mean')
-        my_imshow(logstd, 'logstd')
+        my_imshow(logstd, 'logstd',-1.15,-0.58)
         print('MMSE estimate PSNR: {:.4f}'.format(10*np.log10(np.max(x)**2/np.mean((mn-x)**2))))
         
         my_imsave(x,result_root+'/ground_truth.png')

inexact_pla.py
@@ -60,6 +60,7 @@ class inexact_pla():
         
         # diagnostic checks
         self.logpi_vals = np.zeros((self.n_iter,))
+        self.dgap_vals = np.zeros((self.n_iter,))
         self.num_prox_its_total = 0
     
     def simulate(self, verbose=False):
@@ -101,7 +102,8 @@ class inexact_pla():
             if self.exact:
                 self.x, num_prox_its = self.prox_g(self.x-step_size*self.dfx+np.sqrt(2*step_size)*xi, step_size), 0
             else:
-                self.x, num_prox_its = self.inexact_prox_g(self.x-step_size*self.dfx+np.sqrt(2*step_size)*xi, step_size, epsilon=epsilon_prox, max_iter=iter_prox)
+                # self.x, num_prox_its = self.inexact_prox_g(self.x-step_size*self.dfx+np.sqrt(2*step_size)*xi, step_size, epsilon=epsilon_prox, max_iter=iter_prox)
+                self.x, num_prox_its, self.dgap_vals[self.iter-1] = self.inexact_prox_g(self.x-step_size*self.dfx+np.sqrt(2*step_size)*xi, step_size, epsilon=epsilon_prox, max_iter=iter_prox)
             self.dfx = self.df(self.x)
             self.logpi_vals[self.iter-1] = self.f(self.x) + self.g(self.x)
             if self.iter > self.burnin:

potentials.py
@@ -134,7 +134,6 @@ class l1_loss_unshifted_homoschedastic():
         """
         # auxiliaries for tracking duality gap
         l = 1/2 * np.sum(x**2)
-        C = gamma*self(x)
         i = 0
         stopcrit = False
         
@@ -147,7 +146,7 @@ class l1_loss_unshifted_homoschedastic():
             primal = 1/2*np.sum(y**2) + gamma*self(x-y)
             dual = -1/2*np.sum((x-y)**2) + l
             dgap = primal - dual
-            stopcrit = dgap <= C*epsilon
+            stopcrit = dgap <= epsilon
             i += 1
         return x-y, i
         
@@ -364,7 +363,7 @@ class total_variation():
         tau_agd = 1
         t_agd = 0
         p_prev = np.copy(p)
-        if checkAccuracy: C = gamma * self(u)
+        # if checkAccuracy: C = gamma * self(u)
         
         if verbose: sys.stdout.write('run AGD on dual ROF model: {:3d}% '.format(0)); sys.stdout.flush()
         
@@ -385,6 +384,13 @@ class total_variation():
             p = (1-tau_agd)*q + tau_agd*w
             
             # stopping criterion: check if primal-dual gap < epsilon
+            # div_p = self._imdiv(p)
+            # h = 1/2 * np.sum(div_p**2)
+            # primal = gamma * self(u-div_p) + h
+            # norm_dual_iterate = np.sqrt(np.sum(p**2,axis=0))
+            # dual_inadmissible = np.any(norm_dual_iterate > gamma*self.scale+1e-12)
+            # dual = -np.Inf if dual_inadmissible else - h + np.sum(div_p * u) # dual value. dual iterate should never be inadmissible since we project in the end
+            # dgap = primal-dual
             if checkAccuracy:
                 div_p = self._imdiv(p)
                 h = 1/2 * np.sum(div_p**2)
@@ -393,14 +399,14 @@ class total_variation():
                 dual_inadmissible = np.any(norm_dual_iterate > gamma*self.scale+1e-12)
                 dual = -np.Inf if dual_inadmissible else - h + np.sum(div_p * u) # dual value. dual iterate should never be inadmissible since we project in the end
                 dgap = primal-dual
-                stopcrit = dgap <= C*epsilon
+                stopcrit = dgap <= epsilon
                 if dgap < -5e-15: # for debugging purpose
                     raise ValueError('Duality gap was negative (which should never happen), please check the prox computation routine!')
                 if verbose: sys.stdout.write('\b'*5 + '{:3d}% '.format(int(i/max_iter*100))); sys.stdout.flush()
                 # if verbose and (i%10 == 0 or stopcrit or i==max_iter):
                 #     print('|{:^11d}|{:^31.3e}|'.format(i,dgap))
         if verbose: sys.stdout.write('\b'*5 + '100% '); sys.stdout.flush()
-        return (u - self._imdiv(p)), i
+        return (u - self._imdiv(p)), i, dgap
         
     
 class l2_l1_norm():